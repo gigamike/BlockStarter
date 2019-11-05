d36d25b1590195831dccfe880f6f8f636b68723f
gigamike <michael.gerard.galon@gmail.com>
2019-11-05 12:08:47 +0800

    updated db

--- database/blockstarter.sql
+++ database/blockstarter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 25, 2019 at 10:40 PM
+-- Generation Time: Nov 05, 2019 at 12:08 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.1.31
 
@@ -19,6 +19,278 @@ SET time_zone = "+00:00";
 --
 -- Database: `blockstarter`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE `country` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `country_code` char(2) NOT NULL,
+  `country_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`id`, `country_code`, `country_name`) VALUES
+(1, 'A1', 'Anonymous Proxy'),
+(2, 'A2', 'Satellite Provider'),
+(3, 'O1', 'Other Country'),
+(4, 'AD', 'Andorra'),
+(5, 'AE', 'United Arab Emirates'),
+(6, 'AF', 'Afghanistan'),
+(7, 'AG', 'Antigua and Barbuda'),
+(8, 'AI', 'Anguilla'),
+(9, 'AL', 'Albania'),
+(10, 'AM', 'Armenia'),
+(11, 'AO', 'Angola'),
+(12, 'AP', 'Asia/Pacific Region'),
+(13, 'AQ', 'Antarctica'),
+(14, 'AR', 'Argentina'),
+(15, 'AS', 'American Samoa'),
+(16, 'AT', 'Austria'),
+(17, 'AU', 'Australia'),
+(18, 'AW', 'Aruba'),
+(19, 'AX', 'Aland Islands'),
+(20, 'AZ', 'Azerbaijan'),
+(21, 'BA', 'Bosnia and Herzegovina'),
+(22, 'BB', 'Barbados'),
+(23, 'BD', 'Bangladesh'),
+(24, 'BE', 'Belgium'),
+(25, 'BF', 'Burkina Faso'),
+(26, 'BG', 'Bulgaria'),
+(27, 'BH', 'Bahrain'),
+(28, 'BI', 'Burundi'),
+(29, 'BJ', 'Benin'),
+(30, 'BL', 'Saint Bartelemey'),
+(31, 'BM', 'Bermuda'),
+(32, 'BN', 'Brunei Darussalam'),
+(33, 'BO', 'Bolivia'),
+(34, 'BQ', 'Bonaire, Saint Eustatius and Saba'),
+(35, 'BR', 'Brazil'),
+(36, 'BS', 'Bahamas'),
+(37, 'BT', 'Bhutan'),
+(38, 'BV', 'Bouvet Island'),
+(39, 'BW', 'Botswana'),
+(40, 'BY', 'Belarus'),
+(41, 'BZ', 'Belize'),
+(42, 'CA', 'Canada'),
+(43, 'CC', 'Cocos (Keeling) Islands'),
+(44, 'CD', 'Congo, The Democratic Republic of the'),
+(45, 'CF', 'Central African Republic'),
+(46, 'CG', 'Congo'),
+(47, 'CH', 'Switzerland'),
+(48, 'CI', 'Cote d\'Ivoire'),
+(49, 'CK', 'Cook Islands'),
+(50, 'CL', 'Chile'),
+(51, 'CM', 'Cameroon'),
+(52, 'CN', 'China'),
+(53, 'CO', 'Colombia'),
+(54, 'CR', 'Costa Rica'),
+(55, 'CU', 'Cuba'),
+(56, 'CV', 'Cape Verde'),
+(57, 'CW', 'Curacao'),
+(58, 'CX', 'Christmas Island'),
+(59, 'CY', 'Cyprus'),
+(60, 'CZ', 'Czech Republic'),
+(61, 'DE', 'Germany'),
+(62, 'DJ', 'Djibouti'),
+(63, 'DK', 'Denmark'),
+(64, 'DM', 'Dominica'),
+(65, 'DO', 'Dominican Republic'),
+(66, 'DZ', 'Algeria'),
+(67, 'EC', 'Ecuador'),
+(68, 'EE', 'Estonia'),
+(69, 'EG', 'Egypt'),
+(70, 'EH', 'Western Sahara'),
+(71, 'ER', 'Eritrea'),
+(72, 'ES', 'Spain'),
+(73, 'ET', 'Ethiopia'),
+(74, 'EU', 'Europe'),
+(75, 'FI', 'Finland'),
+(76, 'FJ', 'Fiji'),
+(77, 'FK', 'Falkland Islands (Malvinas)'),
+(78, 'FM', 'Micronesia, Federated States of'),
+(79, 'FO', 'Faroe Islands'),
+(80, 'FR', 'France'),
+(81, 'GA', 'Gabon'),
+(82, 'GB', 'United Kingdom'),
+(83, 'GD', 'Grenada'),
+(84, 'GE', 'Georgia'),
+(85, 'GF', 'French Guiana'),
+(86, 'GG', 'Guernsey'),
+(87, 'GH', 'Ghana'),
+(88, 'GI', 'Gibraltar'),
+(89, 'GL', 'Greenland'),
+(90, 'GM', 'Gambia'),
+(91, 'GN', 'Guinea'),
+(92, 'GP', 'Guadeloupe'),
+(93, 'GQ', 'Equatorial Guinea'),
+(94, 'GR', 'Greece'),
+(95, 'GS', 'South Georgia and the South Sandwich Islands'),
+(96, 'GT', 'Guatemala'),
+(97, 'GU', 'Guam'),
+(98, 'GW', 'Guinea-Bissau'),
+(99, 'GY', 'Guyana'),
+(100, 'HK', 'Hong Kong'),
+(101, 'HM', 'Heard Island and McDonald Islands'),
+(102, 'HN', 'Honduras'),
+(103, 'HR', 'Croatia'),
+(104, 'HT', 'Haiti'),
+(105, 'HU', 'Hungary'),
+(106, 'ID', 'Indonesia'),
+(107, 'IE', 'Ireland'),
+(108, 'IL', 'Israel'),
+(109, 'IM', 'Isle of Man'),
+(110, 'IN', 'India'),
+(111, 'IO', 'British Indian Ocean Territory'),
+(112, 'IQ', 'Iraq'),
+(113, 'IR', 'Iran, Islamic Republic of'),
+(114, 'IS', 'Iceland'),
+(115, 'IT', 'Italy'),
+(116, 'JE', 'Jersey'),
+(117, 'JM', 'Jamaica'),
+(118, 'JO', 'Jordan'),
+(119, 'JP', 'Japan'),
+(120, 'KE', 'Kenya'),
+(121, 'KG', 'Kyrgyzstan'),
+(122, 'KH', 'Cambodia'),
+(123, 'KI', 'Kiribati'),
+(124, 'KM', 'Comoros'),
+(125, 'KN', 'Saint Kitts and Nevis'),
+(126, 'KP', 'Korea, Democratic People\'s Republic of'),
+(127, 'KR', 'Korea, Republic of'),
+(128, 'KW', 'Kuwait'),
+(129, 'KY', 'Cayman Islands'),
+(130, 'KZ', 'Kazakhstan'),
+(131, 'LA', 'Lao People\'s Democratic Republic'),
+(132, 'LB', 'Lebanon'),
+(133, 'LC', 'Saint Lucia'),
+(134, 'LI', 'Liechtenstein'),
+(135, 'LK', 'Sri Lanka'),
+(136, 'LR', 'Liberia'),
+(137, 'LS', 'Lesotho'),
+(138, 'LT', 'Lithuania'),
+(139, 'LU', 'Luxembourg'),
+(140, 'LV', 'Latvia'),
+(141, 'LY', 'Libyan Arab Jamahiriya'),
+(142, 'MA', 'Morocco'),
+(143, 'MC', 'Monaco'),
+(144, 'MD', 'Moldova, Republic of'),
+(145, 'ME', 'Montenegro'),
+(146, 'MF', 'Saint Martin'),
+(147, 'MG', 'Madagascar'),
+(148, 'MH', 'Marshall Islands'),
+(149, 'MK', 'Macedonia'),
+(150, 'ML', 'Mali'),
+(151, 'MM', 'Myanmar'),
+(152, 'MN', 'Mongolia'),
+(153, 'MO', 'Macao'),
+(154, 'MP', 'Northern Mariana Islands'),
+(155, 'MQ', 'Martinique'),
+(156, 'MR', 'Mauritania'),
+(157, 'MS', 'Montserrat'),
+(158, 'MT', 'Malta'),
+(159, 'MU', 'Mauritius'),
+(160, 'MV', 'Maldives'),
+(161, 'MW', 'Malawi'),
+(162, 'MX', 'Mexico'),
+(163, 'MY', 'Malaysia'),
+(164, 'MZ', 'Mozambique'),
+(165, 'NA', 'Namibia'),
+(166, 'NC', 'New Caledonia'),
+(167, 'NE', 'Niger'),
+(168, 'NF', 'Norfolk Island'),
+(169, 'NG', 'Nigeria'),
+(170, 'NI', 'Nicaragua'),
+(171, 'NL', 'Netherlands'),
+(172, 'NO', 'Norway'),
+(173, 'NP', 'Nepal'),
+(174, 'NR', 'Nauru'),
+(175, 'NU', 'Niue'),
+(176, 'NZ', 'New Zealand'),
+(177, 'OM', 'Oman'),
+(178, 'PA', 'Panama'),
+(179, 'PE', 'Peru'),
+(180, 'PF', 'French Polynesia'),
+(181, 'PG', 'Papua New Guinea'),
+(182, 'PH', 'Philippines'),
+(183, 'PK', 'Pakistan'),
+(184, 'PL', 'Poland'),
+(185, 'PM', 'Saint Pierre and Miquelon'),
+(186, 'PN', 'Pitcairn'),
+(187, 'PR', 'Puerto Rico'),
+(188, 'PS', 'Palestinian Territory'),
+(189, 'PT', 'Portugal'),
+(190, 'PW', 'Palau'),
+(191, 'PY', 'Paraguay'),
+(192, 'QA', 'Qatar'),
+(193, 'RE', 'Reunion'),
+(194, 'RO', 'Romania'),
+(195, 'RS', 'Serbia'),
+(196, 'RU', 'Russian Federation'),
+(197, 'RW', 'Rwanda'),
+(198, 'SA', 'Saudi Arabia'),
+(199, 'SB', 'Solomon Islands'),
+(200, 'SC', 'Seychelles'),
+(201, 'SD', 'Sudan'),
+(202, 'SE', 'Sweden'),
+(203, 'SG', 'Singapore'),
+(204, 'SH', 'Saint Helena'),
+(205, 'SI', 'Slovenia'),
+(206, 'SJ', 'Svalbard and Jan Mayen'),
+(207, 'SK', 'Slovakia'),
+(208, 'SL', 'Sierra Leone'),
+(209, 'SM', 'San Marino'),
+(210, 'SN', 'Senegal'),
+(211, 'SO', 'Somalia'),
+(212, 'SR', 'Suriname'),
+(213, 'SS', 'South Sudan'),
+(214, 'ST', 'Sao Tome and Principe'),
+(215, 'SV', 'El Salvador'),
+(216, 'SX', 'Sint Maarten'),
+(217, 'SY', 'Syrian Arab Republic'),
+(218, 'SZ', 'Swaziland'),
+(219, 'TC', 'Turks and Caicos Islands'),
+(220, 'TD', 'Chad'),
+(221, 'TF', 'French Southern Territories'),
+(222, 'TG', 'Togo'),
+(223, 'TH', 'Thailand'),
+(224, 'TJ', 'Tajikistan'),
+(225, 'TK', 'Tokelau'),
+(226, 'TL', 'Timor-Leste'),
+(227, 'TM', 'Turkmenistan'),
+(228, 'TN', 'Tunisia'),
+(229, 'TO', 'Tonga'),
+(230, 'TR', 'Turkey'),
+(231, 'TT', 'Trinidad and Tobago'),
+(232, 'TV', 'Tuvalu'),
+(233, 'TW', 'Taiwan'),
+(234, 'TZ', 'Tanzania, United Republic of'),
+(235, 'UA', 'Ukraine'),
+(236, 'UG', 'Uganda'),
+(237, 'UM', 'United States Minor Outlying Islands'),
+(238, 'US', 'United States'),
+(239, 'UY', 'Uruguay'),
+(240, 'UZ', 'Uzbekistan'),
+(241, 'VA', 'Holy See (Vatican City State)'),
+(242, 'VC', 'Saint Vincent and the Grenadines'),
+(243, 'VE', 'Venezuela'),
+(244, 'VG', 'Virgin Islands, British'),
+(245, 'VI', 'Virgin Islands, U.S.'),
+(246, 'VN', 'Vietnam'),
+(247, 'VU', 'Vanuatu'),
+(248, 'WF', 'Wallis and Futuna'),
+(249, 'WS', 'Samoa'),
+(250, 'YE', 'Yemen'),
+(251, 'YT', 'Mayotte'),
+(252, 'ZA', 'South Africa'),
+(253, 'ZM', 'Zambia'),
+(254, 'ZW', 'Zimbabwe');
 
 -- --------------------------------------------------------
 
@@ -45,9 +317,18 @@ CREATE TABLE `supplier` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text,
-  `eos_public_address` varchar(255) DEFAULT NULL,
+  `public_address` varchar(255) DEFAULT NULL,
+  `created_user_id` bigint(20) UNSIGNED NOT NULL,
   `created_datetime` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `name`, `description`, `public_address`, `created_user_id`, `created_datetime`) VALUES
+(1, 'Malaya Lumber & Construction Supply', 'We offer construction materials, plumbing products, hardware supplies, lumber, plywood, cement, gravel and sand, hollow blocks and paints.', NULL, 6, '2019-11-05 11:46:35'),
+(2, 'Vic\'s Construction & Sons Company', 'The company is a distributor of construction materials, hardware supplies, generator, air compressor, power tools, welding machine, electric motors, etc.', NULL, 7, '2019-11-05 12:06:16');
 
 -- --------------------------------------------------------
 
@@ -57,16 +338,31 @@ CREATE TABLE `supplier` (
 
 CREATE TABLE `user` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `role` enum('admin','supplier','member') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `role` enum('admin','member','supplier') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country_id` int(10) UNSIGNED NOT NULL,
+  `public_address` varchar(255) DEFAULT NULL,
   `salt` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `active` enum('N','Y') NOT NULL,
+  `referrer_user_id` bigint(20) UNSIGNED NOT NULL,
   `created_datetime` datetime NOT NULL,
   `created_user_id` bigint(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `role`, `email`, `password`, `first_name`, `last_name`, `city`, `country_id`, `public_address`, `salt`, `active`, `referrer_user_id`, `created_datetime`, `created_user_id`) VALUES
+(1, 'member', 'manager@gigamike.net', '6d7b0b32a2162d7f14f6490b3bd8ea37', 'Mik', 'Galon', 'Paranaque City', 182, NULL, '$E3rj1QMa_al>W~:ktXuN1~@UT)GF3p$Ci-rp$^N`Ur=P_B!uq', 'Y', 0, '2019-11-05 11:37:25', 0),
+(2, 'member', 'reseller@gigamike.net', 'f1e3d78e930f03908a1ef2b53f235139', 'Amah', 'Galon', 'Paranaque City', 182, NULL, '`+$apw90O>&*U8rRm7tFdQ3YqUL@nZ7;!I\\wyw(dpzZ#h=?kWT', 'Y', 0, '2019-11-05 11:39:39', 0),
+(3, 'member', 'investor@gigamike.net', 'c108e52b49d7e7938e047607b1528c85', 'Zeev', 'Galon', 'Paranaque City', 182, NULL, 'JEv}sj;xTygA.h~T;\\6sj(yB=Wn0/?t(o@OpRDu%W)sone#&>u', 'Y', 0, '2019-11-05 11:40:25', 0),
+(6, 'supplier', 'supplier@gigamike.net', '8fc095c1361b77d0ca16d34da9c31ec3', 'Susan', 'Galon', 'Paranaque City', 182, NULL, 'l?:{x@QZ9ap9w`^BYaX2:Y%]?eU+C!m*/cn*~V68t+_oerAlMh', 'Y', 0, '2019-11-05 11:46:35', 0),
+(7, 'supplier', 'supplier2@gigamike.net', '01f4be4c3bfdf8662a42987285ce5274', 'Vic', 'Galon', 'Makati City', 182, NULL, 'f#C*WQY;(v`v6&PO+ph=QZDR>aTH\'[1F\"MTuHBWev|\'&j>w=\'l', 'Y', 0, '2019-11-05 12:06:16', 0);
 
 --
 -- Indexes for dumped tables
@@ -87,7 +383,8 @@ ALTER TABLE `project`
 ALTER TABLE `supplier`
   ADD PRIMARY KEY (`id`),
   ADD KEY `name` (`name`),
-  ADD KEY `created_datetime` (`created_datetime`);
+  ADD KEY `created_datetime` (`created_datetime`),
+  ADD KEY `created_user_id` (`created_user_id`);
 
 --
 -- Indexes for table `user`
@@ -95,7 +392,14 @@ ALTER TABLE `supplier`
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD KEY `email` (`email`),
-  ADD KEY `role` (`role`);
+  ADD KEY `role` (`role`),
+  ADD KEY `country_id` (`country_id`),
+  ADD KEY `created_datetime` (`created_datetime`),
+  ADD KEY `created_user_id` (`created_user_id`),
+  ADD KEY `first_name` (`first_name`),
+  ADD KEY `last_name` (`last_name`),
+  ADD KEY `city` (`city`),
+  ADD KEY `referrer_user_id` (`referrer_user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -111,13 +415,13 @@ ALTER TABLE `project`
 -- AUTO_INCREMENT for table `supplier`
 --
 ALTER TABLE `supplier`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
